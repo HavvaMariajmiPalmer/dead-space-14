95676d4c93d99ef5eb1942b2a28f87921145c5c1
Алексей Герасимов <gera.com01@mail.ru>
2026-06-19 17:29:18 +0300

    Fix compile error

--- Content.Shared/DeadSpace/Damage/Components/IgnoreContactDamageComponent.cs
+++ Content.Shared/DeadSpace/Damage/Components/IgnoreContactDamageComponent.cs
@@ -1,7 +1,6 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.DeadSpace.Damage.Components;
 
 [RegisterComponent, NetworkedComponent]
-public sealed partial class IgnoreContactDamageComponent : Component
-{
-    
-}
+public sealed partial class IgnoreContactDamageComponent : Component;
